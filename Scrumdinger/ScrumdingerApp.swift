2248d65a310a3fda00b4ac344e717f93ef560df8
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-07-24 00:33:00 +0100

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Ibukunoluwa Akintobi on 24/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
