09949b6cc5873b1cc740470b90f007804abe6fac
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-07-24 13:14:55 +0100

    update:completed main meetingView , added accessibilityValues and labels for elements in MeetingView

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -13,12 +13,37 @@ struct MeetingView: View {
             ProgressView(value:5,total:15)
         }
         HStack{
-            Text("Seconds Elapsed")
+            VStack(alignment:.leading){
+                Text("Seconds Elapsed")
+                    .font(.caption)
+                Label("300", systemImage: "hourglass.tophalf.fill")
+            }
+            Spacer()
+            VStack(alignment:.trailing) {
+                Text("Seconds Remaining")
+                    .font(.caption)
+                Label("600", systemImage: "hourglass.bottomhalf.fill")
+            }
         }
+        .accessibilityElement(children: .ignore)
+        .accessibilityLabel("Time Remaining")
+        .accessibilityValue("10 minutes")
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action:{}){
+                    Image(systemName: "forward.fill")
+                }
+                .accessibilityLabel("Next Speaker")
+            }
+            .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+
+struct Meeting_Previews: PreviewProvider {
     static var previews: some View {
         MeetingView()
     }
