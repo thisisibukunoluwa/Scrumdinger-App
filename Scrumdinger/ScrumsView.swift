533c67196d5fd84eed05aad45001883aae458085
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-07-27 16:32:40 +0100

    feat:added update:added ScrumView list

--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -1,2 +1,18 @@
 import SwiftUI
 
+struct ScrumsView : View {
+    let scrums: [DailyScrum]
+    var body : some View {
+        List(scrums, id: \.title) {
+    // Th closure returns a cardView for each element in the scrums array
+            scrum in CardView(scrum:scrum)
+                .listRowBackground((scrum.theme.mainColor))
+        }
+    }
+}
+
+struct ScrumsView_Previews : PreviewProvider {
+    static var previews : some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
