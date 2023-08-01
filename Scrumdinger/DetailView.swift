429d87c58124214646f27a575474c4f9def57447
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-08-01 23:24:16 +0100

    feat:added toolbar and modal view

--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct DetailView :View {
     
     let scrum : DailyScrum
-    
+    @State private var isPresentingEditView = false
     var body :some View {
         List {
             Section(header: Text("Meeting Info")) {
@@ -43,6 +43,30 @@ struct DetailView :View {
             }
         }
         .navigationTitle(scrum.title)
+        .toolbar{
+            Button("Edit") {
+                isPresentingEditView = true
+            }
+        }
+        .sheet(isPresented: $isPresentingEditView) {
+            NavigationStack {
+                DetailEditView()
+                    .navigationTitle(scrum.title)
+                    .toolbar {
+                        ToolbarItem(placement: .cancellationAction) {
+                            Button("Cancel") {
+                                isPresentingEditView = false
+                            }
+                        }
+                        ToolbarItem(placement: .cancellationAction) {
+                            Button("Done") {
+                                isPresentingEditView = false
+                            }
+                    }
+
+                }
+            }
+        }
     }
 }
 
