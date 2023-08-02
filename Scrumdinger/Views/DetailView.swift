678916d37fa21596b8ee23bba6c21cf2f163e81c
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-08-02 17:07:45 +0100

    update:changed all static non mutable values to use bindings, added state bindings

--- Scrumdinger/Views/DetailView.swift
+++ Scrumdinger/Views/DetailView.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 struct DetailView :View {
     
-    let scrum : DailyScrum
+    @Binding var scrum:DailyScrum
     @State private var isPresentingEditView = false
+    @State private var editingScrum = DailyScrum.emptyScrum
+    
     var body :some View {
         List {
             Section(header: Text("Meeting Info")) {
@@ -46,11 +48,12 @@ struct DetailView :View {
         .toolbar{
             Button("Edit") {
                 isPresentingEditView = true
+                editingScrum = scrum
             }
         }
         .sheet(isPresented: $isPresentingEditView) {
             NavigationStack {
-                DetailEditView()
+                DetailEditView(scrum:$editingScrum)
                     .navigationTitle(scrum.title)
                     .toolbar {
                         ToolbarItem(placement: .cancellationAction) {
@@ -61,6 +64,7 @@ struct DetailView :View {
                         ToolbarItem(placement: .cancellationAction) {
                             Button("Done") {
                                 isPresentingEditView = false
+                                scrum = editingScrum
                             }
                     }
 
@@ -74,7 +78,7 @@ struct DetailView :View {
 struct DetailView_Previews : PreviewProvider {
     static var previews: some View {
         NavigationStack {
-            DetailView(scrum: DailyScrum.sampleData[0])
+            DetailView(scrum: .constant(DailyScrum.sampleData[0]))
         }
     }
 }
