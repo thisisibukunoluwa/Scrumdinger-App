08fb38dc970497f8a0e1311271e81658e4cc8d27
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-08-02 17:06:56 +0100

    update:changed all static non mutable values to use bindings

--- Scrumdinger/Views/DetailEditView.swift
+++ Scrumdinger/Views/DetailEditView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct DetailEditView:View {
-    @State private var scrum = DailyScrum.emptyScrum
+    @Binding var scrum:DailyScrum
     @State private var newAttendeeName = ""
     var body:some View {
         Form {
@@ -23,6 +23,7 @@ struct DetailEditView:View {
                     Text("\(scrum.lengthInMinutes)")
                         .accessibilityHidden(true)
                 }
+                ThemePicker(selection: $scrum.theme)
             }
             Section(header:Text("Attendees")) {
                 ForEach(scrum.attendees) {
@@ -52,7 +53,7 @@ struct DetailEditView:View {
 
 struct DetailEditView_Previews : PreviewProvider {
     static var previews: some View {
-        DetailEditView()
+        DetailEditView(scrum: .constant(DailyScrum.sampleData[0]))
     }
 }
 
