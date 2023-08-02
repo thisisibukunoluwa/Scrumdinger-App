7f47306560d0aee064c9f2740e1e627cf7c020b0
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-08-02 17:09:21 +0100

    feat:crated ThemePicker, added logic to iterte over all themvalues

--- Scrumdinger/Views/ThemePicker.swift
+++ Scrumdinger/Views/ThemePicker.swift
@@ -5,4 +5,25 @@
 //  Created by Ibukunoluwa Akintobi on 02/08/2023.
 //
 
-import Foundation
+import SwiftUI
+
+struct ThemePicker:View {
+    @Binding var selection:Theme
+    
+    var body:some View {
+        Picker("Theme",selection: $selection){
+            ForEach(Theme.allCases){
+                theme in ThemeView(theme: theme)
+                    .tag(theme)
+            }
+        }
+        .pickerStyle(.navigationLink)
+    }
+}
+
+
+struct ThemePicker_Previews : PreviewProvider {
+    static var previews : some View {
+        ThemePicker(selection: .constant(.periwinkle))
+    }
+}
