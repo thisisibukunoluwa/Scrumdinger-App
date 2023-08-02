df6916efca77eb406e69ddcb2b411a341d9df6fd
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-08-02 17:10:38 +0100

    feat:ad ded ThemeView.swift

--- Scrumdinger/Views/ThemeView.swift
+++ Scrumdinger/Views/ThemeView.swift
@@ -4,5 +4,28 @@
 //
 //  Created by Ibukunoluwa Akintobi on 02/08/2023.
 //
+import SwiftUI
 
-import Foundation
+struct ThemeView: View {
+    
+    let theme:Theme
+    
+    var body : some View {
+        Text(theme.name)
+            .padding(4)
+            .frame(maxWidth: .infinity)
+            .background(theme.mainColor)
+            .foregroundColor(theme.accentColor)
+            .clipShape(RoundedRectangle(cornerRadius: 4))
+    }
+}
+
+struct ThemeView_Previews : PreviewProvider {
+    static var previews : some View {
+        ThemeView(theme: .buttercup)
+    }
+}
+
+
+
+
